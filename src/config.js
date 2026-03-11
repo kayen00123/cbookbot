c1276cd76349976d635603d8d0f0fe55997ac972
Dominic Otterfi <tokenlisting79@gmail.com>
2026-03-11 13:46:07 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,13 +4,13 @@ const config = {
   // Bot Settings
   bot: {
     timezone: process.env.TIMEZONE || 'UTC',
-    scheduledTimes: (process.env.SCHEDULED_TIMES || '09:45,13:00,18:12,11:30,12:08,12:14,20:10,20:17')
+    scheduledTimes: (process.env.SCHEDULED_TIMES || '09:45,14:00,18:12,22:17')
       .split(',')
       .map(t => t.trim()),
-    hashtagTimes: (process.env.HASHTAG_TIME || '07:16,09:16,11:16,13:16,18:16,20:28')
+    hashtagTimes: (process.env.HASHTAG_TIME || '12:16,16:38')
       .split(',')
       .map(t => t.trim()),
-    accountTimes: (process.env.ACCOUNT_TIME || '08:27,10:30,15:00,20:38')
+    accountTimes: (process.env.ACCOUNT_TIME || '13:27,20:38')
       .split(',')
       .map(t => t.trim()),
     enableRandomMode: process.env.ENABLE_RANDOM_MODE !== 'false',
